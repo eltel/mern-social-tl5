ec6c64bf58e1391628d1eae67c69a20e4d2e1192
eltel <djterrymitchell@gmail.com>
2022-07-14 14:51:21 -0300

    removed config.env from git, updated baseUrl for production

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,9 @@
-// const baseUrl ="http://localhost:3000";
+const baseUrl =
+  process.env.NODE_ENV !== "production"
+    ? "http://localhost:3000"
+    : "https://mern-social-tl5.herokuapp.com";
 
 // Production
-const baseUrl = "https://mern-social-tl5.vercel.app";
+// const baseUrl = "https://mern-social-tl5.vercel.app";
 
 export default baseUrl;
