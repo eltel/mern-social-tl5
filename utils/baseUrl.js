0a84b8e32891f8b365776dcf6b950214f6d89c66
eltel <djterrymitchell@gmail.com>
2022-07-14 13:07:01 -0300

    resolved baseUrl error

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,3 +1,6 @@
-const baseUrl ="http://localhost:3000";
+// const baseUrl ="http://localhost:3000";
+
+// Production
+const baseUrl = "https://mern-social-tl5.vercel.app";
 
 export default baseUrl;
