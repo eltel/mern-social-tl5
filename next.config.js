75bb6548a1afa7d556f85f7835396a8ab90dee3b
eltel <djterrymitchell@gmail.com>
2022-07-14 12:48:35 -0300

    second commit

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   env: {
-    CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/setstate/tl5",
+    CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/setstate/image/upload",
   },
 };
+
+// https://api.cloudinary.com/v1_1/setstate/tl5
