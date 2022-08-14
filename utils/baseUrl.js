a99945e483f538117a62fdf061ab4ffbd235df8f
eltel <djterrymitchell@gmail.com>
2022-08-14 11:54:13 -0300

    fix for testing Heroku pipeline-testing url fix

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,7 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "https://mern-social-tl5.herokuapp.com";
+    : "https://tl5-preview-testing-kzi6dwegwo.herokuapp.com";
+// : "https://mern-social-tl5.herokuapp.com";
 
 module.exports = baseUrl;
